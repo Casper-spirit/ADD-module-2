6d85a476bb83864f16d194836134d3722e9cfcd7
Casper-spirit <alaid.5@mail.ru>
2024-11-20 21:37:32 +1200

    Update Дополнительное.py

--- Дополнительное.py
+++ Дополнительное.py
@@ -5,9 +5,12 @@ import random
 first_numb = random.randint(3, 20)
 print("First_number: ",  first_numb)
 x = int(first_numb)
+sec_res = []
 for i in range(1, 21):
     for j in range(1, 21):
         if j + i == x and i != j:
-            print('Second_nun: ', i,"and", j)
+            sec_number = i, j
+            #print('Second_nun: ', sec_number)
 
-
+            sec_res.extend(sec_number)
+print('Second_nun: ',sec_res)
